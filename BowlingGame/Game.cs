69af01c7f0fa172cc09baa7b4b14cc4ceadf0e7f
AlekseySugrobov <Aleksey.Sugrobov@gmail.com>
2017-08-29 18:29:58 +0500

    firstRoll and secondRoll testing

---
+++ BowlingGame/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace BowlingGame
+{
+    public class Game
+    {
+        public int Score { get; set; }
+
+        public Game()
+        {
+            Score = 0;
+        }
+        public void Roll(int pins)
+        {
+            Score += pins;
+        }
+
+    }
+}
---
+++ BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+
+namespace BowlingGameTests
+{
+    [TestFixture]
+    public class BowlingGameTests
+    {
+        private Game game;
+
+
+        [Test]
+        public void InitGame()
+        {
+            game = new Game();
+            Assert.IsNotNull(game);
+        }
+
+        [Test]
+        public void TestHittinZeroPerRoll()
+        {
+            game = new Game();
+            RollPins(20, 0);
+            Assert.AreEqual(0, game.Score);
+        }
+
+        [Test]
+        public void TestHittingOnePerRoll()
+        {
+            game = new Game();
+            RollPins(20,1);
+            Assert.AreEqual(20, game.Score);
+        }
+
+        private void RollPins(int numberOfRolls, int pinsHitPerRoll)
+        {
+            for (var i = 0; i < numberOfRolls; i++)
+            {
+                game.Roll(pinsHitPerRoll);
+            }
+        }
+    }
+}
